c901f0651781ba8bab4551ec2f40fc43e386f78c
Cristian Cotes <cotes.cristian@gmail.com>
2016-05-27 12:48:37 +0200

    Revert "Remove recent file list from libreoffice."
    This reverts commit d729e711cf8d7eeec098b535aa042dfdd3e725d8.

--- scripts/closeApps.py
+++ scripts/closeApps.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import os
 import uno;
 from subprocess import call;
 from os.path import expanduser;
@@ -52,4 +51,3 @@ def connectAndDestroy( port ):
 	return;
 
 connectAndDestroy("2002");
-os.system("sed -i.bck '/PickList/d' /home/user/.config/libreoffice/4/user/registrymodifications.xcu")
