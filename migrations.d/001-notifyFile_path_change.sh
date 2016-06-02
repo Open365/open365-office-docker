77a049f87229d347ba53621c76d5185d15848e71
Jordi Nistal <jordi.nistal@gmail.com>
2016-06-02 13:05:57 +0200

    Create libreoffice migration "notifyFilePathChanged"
    Copies the modified libreoffice config files to the users that do not
    have them.

---
+++ migrations.d/001-notifyFile_path_change.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Add macro to libreoffice
+# This macro is executed "on document title change" and sends through
+# the bus the current file path
+
+LO_SKEL_FOLDER="/etc/.skel/.config/libreoffice"
+LO_USER_CONFIG_FOLDER="/home/user/.config/libreoffice"
+
+if [ -d "$LO_USER_CONFIG_FOLDER" ] && ! grep -qr "notifyFilePathChanged" "$LO_USER_CONFIG_FOLDER"
+then
+    echo "Running migration: libreOffice notifyFilePathChange"
+    cp "$LO_SKEL_FOLDER"/4/user/registrymodifications.xcu "$LO_USER_CONFIG_FOLDER"/4/user/registrymodifications.xcu
+    cp "$LO_SKEL_FOLDER"/4/user/basic/Standard/Module1.xba "$LO_USER_CONFIG_FOLDER"/4/user/basic/Standard/Module1.xba
+    echo "Migration done"
+else
+    echo "libreOffice notifyFilePathChange migration: Nothing to migrate"
+fi
