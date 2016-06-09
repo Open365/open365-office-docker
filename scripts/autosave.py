eb9cd8878b15c9df9dd09dbecb16a76d5a161b3e
Ferran Mesas <ferran.mesas@eyeos.com>
2016-06-09 15:34:47 +0200

    Create a new connection to libreOffice each time
    This avoids some race conditions that can make libreoffice crash

--- scripts/autosave.py
+++ scripts/autosave.py
@@ -9,8 +9,8 @@ autosave_time = float(os.environ.get('AUTOSAVE_TIME', 60))
 
 if __name__ == "__main__":
 
-    desktop = LOffice.connect()
 
     while True:
+        desktop = LOffice.connect()
         LOffice.saveAllDocs(desktop)
         time.sleep(autosave_time)
